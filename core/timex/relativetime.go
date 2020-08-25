43ced8dc88368977076a2773c35ad7ab5d48c93d
grzeng-go <61483296+grzeng-go@users.noreply.github.com>
2020-08-25 16:40:19 +0800

    timex包的作用注释

--- core/timex/relativetime.go
+++ core/timex/relativetime.go
@@ -2,6 +2,7 @@ package timex
 
 import "time"
 
+// 该包的作用是避免了系统调用（time.Now()）
 // Use the long enough past time as start time, in case timex.Now() - lastTime equals 0.
 var initTime = time.Now().AddDate(-1, -1, -1)
 
